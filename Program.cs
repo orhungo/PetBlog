d3907a5f67f46fdfba55ea2541bbecfcda754767
Gökcen ORHUN <131930072@ogr.uludag.edu.tr>
2025-01-24 23:09:54 +0300

    UPD

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-// Session için
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -14,22 +14,21 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-// DbContext'i servislere ekledim
+
 builder.Services.AddDbContext<MyAppContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseSession(); // Session middleware
+app.UseSession(); 
 app.UseRouting();
 
 app.UseAuthorization();
